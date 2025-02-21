ac8e12f795df48d977144bd33a161d5783b6fcf2
NadFuentes <nadfuentes03@unitec.edu>
2025-02-21 13:30:57 -0600

    Examen Parcial I

---
+++ Examen_Parcial_I/src/examen_parcial_i/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen_parcial_i;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author Nadiesda Fuentes
+ */
+public class Main{
+    public static void main(String[] args) {
+        // Crear la instancia de UberSocial
+        UberSocial uberSocial = new UberSocial();
+        
+    }
+}
