6ca90cbe46e29a806388aa6e913e920e376aa209
NadFuentes <nadfuentes03@unitec.edu>
2025-02-21 14:17:45 -0600

    Examen Parcial I

---
+++ Examen_Parcial_I/src/examen_parcial_i/AgregarCuenta.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_parcial_i;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Nadiesda Fuentes
+ */
+public class AgregarCuenta extends javax.swing.JFrame {
+
+    public AgregarCuenta() {
+        initComponents();
+        setLocationRelativeTo(null);
+    }
+
+   
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField tipoField;
+    private javax.swing.JTextField usernameField;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ Examen_Parcial_I/src/examen_parcial_i/AgregarPost.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen_parcial_i;
+
+/**
+ *
+ * @author Nadiesda Fuentes
+ */
+public class AgregarPost extends javax.swing.JFrame {
+
+   
+    public AgregarPost() {
+        initComponents();
+          setLocationRelativeTo(null);
+    }
+
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Volver;
+    private javax.swing.JButton agregar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JTextField postField;
+    private javax.swing.JTextField usernameField;
+    // End of variables declaration//GEN-END:variables
+}
